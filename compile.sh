300a74a19ac56b28e43d5ee98e8126d0c3dd0c4f
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2018-08-06 15:56:30 +0300

    Find if version is bigger than FFMpeg 4.0.1 and we'll use SWResample not AVResample in Bash compile and indent BASH file

--- compile.sh
+++ compile.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
-RESAMPLER=__LIBAVRESAMPLE__ 
-#RESAMPLER=__LIBSWRESAMPLE__ 
-#RESAMPLER=__FFMPEGOLDAPI__ 
-RESAMPLER_LIB=avresample
-# RESAMPLER_LIB=swresample
-# RESAMPLER_LIB=  
+#!/bin/bash
 
-gcc -o ffmpeg-example-bin example1.c example2.c example3.c example4.c main.c -l${RESAMPLER_LIB} -lavcodec -lavformat -lavutil -lm \
--g -D${RESAMPLER} -std=c99 -I.
+RESAMPLER=_"_LIBAVRESAMPLE__"
+#RESAMPLER="__LIBSWRESAMPLE__"
+#RESAMPLER="__FFMPEGOLDAPI__"
+RESAMPLER_LIB="avresample"
+# RESAMPLER_LIB="swresample"
+# RESAMPLER_LIB=
+
+FFMPEG_INCLUDE=/usr/include/ffmpeg
+FFMPEG_VERSION=$(pkg-config --modversion libavcodec | tr -d '.')
+
+if [ ${FFMPEG_VERSION} -ge 5818100 ]
+then
+    echo "FFMpeg Version is bigger than 4.0.1. Which to SWResample because AVResample is depricated."
+    RESAMPLER_LIB="swresample"
+    RESAMPLER="__LIBSWRESAMPLE__"
+fi
+
+gcc -o ffmpeg-example-bin example1.c example2.c example3.c example4.c main.c -l${RESAMPLER_LIB} \
+    -lavcodec -lavformat -lavutil -lm -g -D${RESAMPLER} -std=c99 -I. -I${FFMPEG_INCLUDE}
